61d72c60ed3f3eb026db937eee66796ba9c15658
bvds <bennyvds.stratx@gmail.com>
2021-11-30 14:34:42 +0100

    published version 1.0.2 - added more formatting

--- dateTimeFormatter.js
+++ dateTimeFormatter.js
@@ -8,22 +8,17 @@ function formattedNumber(nbr){
     return formattedNbr;
 }
 
-
 function dtLogger(_prefix){
     var currentDT = new Date;
-    let dag = currentDT.getDate();
-    if (dag < 10) { 
-        dag = "0" + dag;
-    }
 
-    let maand = currentDT.getMonth()+1; //January is month 0 ...
-    let uur = currentDT.getHours();
-    let minuten = formattedNumber(currentDT.getMinutes());
-    
-    let sekonden = currentDT.getSeconds();
-    let milisek = currentDT.getMilliseconds();
+    let dag = formattedNumber(currentDT.getDate());
+    let maand = formattedNumber(currentDT.getMonth()+1); //January is month 0 ...
+    let uur = formattedNumber(currentDT.getHours());
+    let minuten = formattedNumber(currentDT.getMinutes()); 
+    let sekonden = formattedNumber(currentDT.getSeconds());
+    let milisek = formattedNumber(currentDT.getMilliseconds());
     let formattedDT = _prefix + maand + "-" + dag + " " + uur +":"+minuten+":"+sekonden+":"+milisek
-    //console.log("this function will format " + currentDT + " into " +formattedDT);
+    
     return formattedDT;
 }
 

f7862bf233a1efb05c5465319f5106480e890824
bvds <bennyvds.stratx@gmail.com>
2021-12-03 11:18:33 +0100

    Update dateTimeFormatter.js
    adding support for nbr of digits. Default 2, optional 3 (for milliseconds)

--- dateTimeFormatter.js
+++ dateTimeFormatter.js
@@ -1,22 +1,40 @@
-function formattedNumber(nbr){
-    if (nbr < 10) {
-        var formattedNbr = "0" + nbr;
-    }
+function formattedNumber(nbr,digits="2"){ //if no nbr of digits send use default 2
+    if (digits === "2") {
+        if (nbr < 10) {
+            var formattedNbr = "0" + nbr;
+        }
+        else {
+            var formattedNbr = nbr;
+        }
+    } 
     else {
-        var formattedNbr = nbr;
+        if (digits === "3") {
+            if (nbr < 10) {
+                var formattedNbr = "00" + nbr;
+            }
+            else {
+                if (nbr < 100) {
+                    var formattedNbr = "0" + nbr;
+                }
+                else {
+                    var formattedNbr = nbr;
+                }
+            }
+        } else { //in case of different number of digits, just return the number
+            var formattedNbr = nbr;
+        }
     }
     return formattedNbr;
 }
 
 function dtLogger(_prefix){
     var currentDT = new Date;
-
     let dag = formattedNumber(currentDT.getDate());
     let maand = formattedNumber(currentDT.getMonth()+1); //January is month 0 ...
     let uur = formattedNumber(currentDT.getHours());
     let minuten = formattedNumber(currentDT.getMinutes()); 
     let sekonden = formattedNumber(currentDT.getSeconds());
-    let milisek = formattedNumber(currentDT.getMilliseconds());
+    let milisek = formattedNumber(currentDT.getMilliseconds(),"4");
     let formattedDT = _prefix + maand + "-" + dag + " " + uur +":"+minuten+":"+sekonden+":"+milisek
     
     return formattedDT;

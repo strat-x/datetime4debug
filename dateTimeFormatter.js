430d7cf5e13c42ac1b35c9a1e1bc73dc6cea2c41
bvds <bennyvds.stratx@gmail.com>
2021-12-05 13:18:41 +0100

    voorbeeldje

--- dateTimeFormatter.js
+++ dateTimeFormatter.js
@@ -1,6 +1,9 @@
 function formattedNumber(nbr,digits=2){ //if no nbr of digits send use default 2
     var formattedNbr = "";
 
+    var mariskaName = "Mariska";
+    
+
     if (typeof(nbr) === "string") { //in case a string is provided iso number, return the string
         formattedNbr = nbr;
     }
